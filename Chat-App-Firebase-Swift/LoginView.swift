974f51889938279e19565a3e7bce7df888fe51cd
ermallimaj <ermallimaj32@gmail.com>
2024-02-26 06:51:55 -0800

    login-authorization

--- Chat-App-Firebase-Swift/LoginView.swift
+++ Chat-App-Firebase-Swift/LoginView.swift
@@ -6,35 +6,58 @@
 //
 
 import SwiftUI
+import Firebase
+
+class FirebaseManager: NSObject {
+    
+    let auth: Auth
+    
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        
+        self.auth = Auth.auth()
+        
+        super.init()
+    }
+    
+}
 
 struct LoginView: View {
     
     @State private var isLoginMode = false
-    
+
     @State private var email = ""
-    
+
     @State private var password = ""
+
+    @State private var firstname = ""
+
+    @State private var lastname = ""
     
     var body: some View {
         NavigationView {
             ScrollView {
-                
                 VStack(spacing: 16) {
                     Picker(selection: $isLoginMode, label: Text("Picker here")) {
-                        Text("Login")
-                            .tag(true)
-                        Text("Create Account")
-                            .tag(false)
+                        Text("Login").tag(true)
+                        Text("Create Account").tag(false)
                     }.pickerStyle(SegmentedPickerStyle())
-                        
+                    
                     if !isLoginMode {
-                        Button(action: {
-                            // Action for creating account
-                        }) {
+                        Button(action: {}) {
                             Image(systemName: "person.fill")
+                                .foregroundColor(.black)
                                 .font(.system(size: 64))
                                 .padding()
                         }
+                        Group {
+                            TextField("First name", text: $firstname)
+                            TextField("Last name", text: $lastname)
+                        }
+                        .padding(12)
+                        .background(Color.white)
                     }
                     
                     Group {
@@ -57,9 +80,12 @@ struct LoginView: View {
                                 .padding(.vertical, 10)
                                 .font(.system(size: 14, weight: .semibold))
                             Spacer()
-                        }.background(Color.blue)
+                        }.background(isLoginMode ? Color.blue : Color.green)
                         
                     }
+                    
+                    Text(self.loginStatusMessage)
+                        .foregroundColor(.red)
                 }
                 .padding()
                 
@@ -68,18 +94,51 @@ struct LoginView: View {
             .background(Color(.init(white: 0, alpha: 0.05))
                             .ignoresSafeArea())
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
     
     private func handleAction() {
         if isLoginMode {
-            print("Should log in with existing credentials!")
+            loginUser()
         } else {
-            print("Register a new account!")
+            createNewAccount()
+        }
+    }
+    
+    private func loginUser() {
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) { result, err in
+            if let err = err {
+                print("Failed to login user:", err)
+                self.loginStatusMessage = "Failed to login user, should login using existing credentials"
+                return
+            }
+            
+            print("Successfully logged in as user: \(result?.user.uid ?? "")")
+            
+            self.loginStatusMessage = "Successfully logged in!"
+        }
+    }
+    
+    @State private var loginStatusMessage = ""
+    
+    private func createNewAccount() {
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) { result, err in
+            if let err = err {
+                print("Failed to create user:", err)
+                self.loginStatusMessage = "Failed to create user!"
+                return
+            }
+            // will be saved later to the database along with the email and password
+            let userData = ["firstname": firstname, "lastname": lastname]
+            
+            print("Successfully created user: \(result?.user.uid ?? "")")
+            
+            self.loginStatusMessage = "Successfully created user!"
         }
     }
 }
 
-struct ContentView_Previews1: PreviewProvider {
+struct Previews_LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
     }
