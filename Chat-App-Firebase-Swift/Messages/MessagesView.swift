0cec4b5be71cb3c0545af8cb735388518111c623
Genti1bytyqi <gentrit.bytyci1@student.uni-pr.edu>
2024-02-27 20:19:16 +0100

    user-can-now-fetch-user-data

--- Chat-App-Firebase-Swift/Messages/MessagesView.swift
+++ Chat-App-Firebase-Swift/Messages/MessagesView.swift
@@ -6,115 +6,139 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
+
+struct ChatUser {
+    let uid, firstname, lastname, email, profileImageUrl: String
+}
 
 struct MessagesView: View {
-    @State var showLogOut = false
-        
-        private var customNavBar: some View {
-            HStack(spacing: 16) {
-                
-                Image(systemName: "person.fill")
-                    .font(.system(size: 34, weight: .heavy))
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("USERNAME")
-                        .font(.system(size: 24, weight: .bold))
-                    
-                    HStack {
-                        Circle()
-                            .foregroundColor(.green)
-                            .frame(width: 14, height: 14)
-                        Text("online")
-                            .font(.system(size: 12))
-                            .foregroundColor(Color(.lightGray))
-                    }
-                    
-                }
-                
-                Spacer()
-                Button {
-                    showLogOut.toggle()
-                } label: {
-                    Image(systemName: "gear")
-                        .font(.system(size: 24, weight: .bold))
-                        .foregroundColor(Color(.label))
-                }
+    
+    @State var showLogOLut = false
+    
+    @ObservedObject private var vm = MainMessagesViewModel()
+    
+    var body: some View {
+        NavigationView {
+            
+            VStack {
+                customNavBar
+                messagesView
             }
-            .padding()
-            .actionSheet(isPresented: $showLogOut) {
-                .init(title: Text("Settings"), message: Text("What's your decision?"), buttons: [
-                    .destructive(Text("Sign Out"), action: {
-                        print("user decided to sign out")
-                    }),
-                        .cancel()
-                ])
-            }
-        }
-        
-        var body: some View {
-            NavigationView {
-                
-                VStack {
-                    customNavBar
-                    messagesView
-                }
-                .overlay(
-                    newMessageButton, alignment: .bottom)
-                .navigationBarHidden(true)
-            }
-        }
-        
-        private var messagesView: some View {
-            ScrollView {
-                ForEach(0..<10, id: \.self) { num in
-                    VStack {
-                        HStack(spacing: 16) {
-                            Image(systemName: "person.fill")
-                                .font(.system(size: 32))
-                                .padding(8)
-                                .overlay(RoundedRectangle(cornerRadius: 44)
-                                            .stroke(Color(.label), lineWidth: 1)
-                                )
-                            
-                            
-                            VStack(alignment: .leading) {
-                                Text("Username")
-                                    .font(.system(size: 16, weight: .bold))
-                                Text("Message sent to user")
-                                    .font(.system(size: 14))
-                                    .foregroundColor(Color(.lightGray))
-                            }
-                            Spacer()
-                            
-                            Text("22d")
-                                .font(.system(size: 14, weight: .semibold))
-                        }
-                        Divider()
-                            .padding(.vertical, 8)
-                    }.padding(.horizontal)                    
-                }.padding(.bottom, 50)
-            }
-        }
-        
-        private var newMessageButton: some View {
-            Button {
-                
-            } label: {
-                HStack {
-                    Spacer()
-                    Text("+ New Message")
-                        .font(.system(size: 16, weight: .bold))
-                    Spacer()
-                }
-                .foregroundColor(.white)
-                .padding(.vertical)
-                    .background(Color.blue)
-                    .cornerRadius(32)
-                    .padding(.horizontal)
-                    .shadow(radius: 15)
-            }
+            .overlay(
+                newMessageButton, alignment: .bottom)
+            .navigationBarHidden(true)
         }
     }
+    
+    private var customNavBar: some View {
+        HStack(spacing: 16) {
+            
+            WebImage(url: URL(string: vm.chatUser?.profileImageUrl ?? ""))
+                .resizable()
+                .scaledToFill()
+                .frame(width: 50, height: 50)
+                .clipped()
+                .cornerRadius(50)
+                .overlay(RoundedRectangle(cornerRadius: 44)
+                            .stroke(Color(.label), lineWidth: 1)
+                )
+                .shadow(radius: 5)
+            
+            
+            VStack(alignment: .leading, spacing: 4) {
+                let firstname = vm.chatUser?.firstname ?? ""
+                
+                let lastname = vm.chatUser?.lastname ?? ""
+                
+                let fullName = firstname + " " + lastname
+                
+                Text(fullName)
+                    .font(.system(size: 24, weight: .bold))
+                
+                HStack {
+                    Circle()
+                        .foregroundColor(.green)
+                        .frame(width: 14, height: 14)
+                    Text("Online")
+                        .font(.system(size: 13))
+                        .foregroundColor(Color(.lightGray))
+                }
+                
+            }
+            
+            Spacer()
+            Button {
+                showLogOLut.toggle()
+            } label: {
+                Image(systemName: "gear")
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundColor(Color(.label))
+            }
+        }
+        .padding()
+        .actionSheet(isPresented: $showLogOLut) {
+            .init(title: Text("Settings"), message: Text("What's your decision?"), buttons: [
+                .destructive(Text("Sign Out"), action: {
+                    print("user decided to sign out")
+                }),
+                    .cancel()
+            ])
+        }
+    }
+    
+    private var messagesView: some View {
+        ScrollView {
+            ForEach(0..<10, id: \.self) { num in
+                VStack {
+                    HStack(spacing: 16) {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 32))
+                            .padding(8)
+                            .overlay(RoundedRectangle(cornerRadius: 44)
+                                        .stroke(Color(.label), lineWidth: 1)
+                            )
+                        
+                        
+                        VStack(alignment: .leading) {
+                            Text("Username")
+                                .font(.system(size: 16, weight: .bold))
+                            Text("Message sent to user")
+                                .font(.system(size: 14))
+                                .foregroundColor(Color(.gray))
+                        }
+                        Spacer()
+                        
+                        Text("22d")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    Divider()
+                        .padding(.vertical, 8)
+                }.padding(.horizontal)
+                
+            }.padding(.bottom, 50)
+        }
+    }
+    
+    private var newMessageButton: some View {
+        Button {
+            
+        } label: {
+            HStack {
+                Spacer()
+                Text("New Message")
+                    .font(.system(size: 16, weight: .bold))
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .padding(.vertical)
+                .background(Color.green)
+                .cornerRadius(32)
+                .padding(.horizontal)
+                .shadow(radius: 15)
+        }
+    }
+}
 
 struct MessagesView_Previews: PreviewProvider {
     static var previews: some View {
